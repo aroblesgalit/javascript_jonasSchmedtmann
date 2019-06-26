5191634cadfeb0cbecbc47bb288b014bd2a54acb
Alvin Galit <aroblesgalit@gmail.com>
2019-06-25 19:41:38 -0700

    followed along the instructor's solution to CODING CHALLENGE 2

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -346,7 +346,7 @@ GOOD LUCK :)
 /*
 My solution
 */
-
+/*
 // 1
 var avgJohn = (89 + 120 + 103) / 3;
 var avgMike = (116 + 94 + 123) / 3;
@@ -405,6 +405,38 @@ if (avgJohn > avgMike && avgJohn > avgMary) {
 } else {
     console.log('Mike\'s team wins in average with a value of ' + avgMike);
 }
+*/
+
+
+/*
+Instructor's solution
+*/
+
+var scoreJohn = (189 + 120 + 103) / 3;
+var scoreMike = (129 + 94 + 123) / 3;
+var scoreMary = (97 + 134 + 105) / 3;
+console.log(scoreJohn, scoreMike, scoreMary);
+
+if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
+    console.log('John\'s team wins with ' + scoreJohn + ' points');
+} else if (scoreMike > scoreJohn && scoreMike > scoreMary) {
+    console.log('Mike\'s team wins with ' + scoreMike + ' points');
+} else if (scoreMary > scoreJohn && scoreMary > scoreMike) {
+    console.log('Mary\'s team wins with ' + scoreMary + ' points');
+} else {
+    console.log('There is a draw');
+}
+
+/*
+if (scoreJohn > scoreMike) {
+    console.log('John\'s team wins with ' + scoreJohn + ' points');
+} else if (scoreMike > scoreJohn) {
+    console.log('Mike\'s team wins with ' + scoreMike + ' points');
+} else {
+    console.log('There is a draw');
+}
+*/
+
 
 
 
