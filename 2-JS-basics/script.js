a250246b2ee4469d36cf570d8f421812c148ad79
Alvin Galit <aroblesgalit@gmail.com>
2019-06-27 09:25:31 -0700

    learned about objects and methods, created a method for calculating current age and then returning a new key-value pair

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -608,7 +608,8 @@ console.log(tips, finalValues);
 /******************************
 * Objects and properties
 */
-
+/*
+// Object literal
 var john = {
     firstName: 'John',
     lastName: 'Smith',
@@ -626,11 +627,34 @@ john.job = 'designer';
 john['isMarried'] = true;
 console.log(john);
 
+// new Object syntax
 var jane = new Object();
 jane.firstName = 'Jane';
 jane.birthYear = 1969;
 jane['lastName'] = 'Smith';
 console.log(jane);
+*/
+
+
+
+/******************************
+* Objects and methods
+*/
+
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function() {
+        this.age = 2018 - this.birthYear;
+    }
+};
+
+john.calcAge();
+console.log(john);
 
 
 
