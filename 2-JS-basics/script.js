7a783d9617d231aa3835936b7c7fc02f3adeceb4
Alvin Galit <aroblesgalit@gmail.com>
2019-06-26 20:59:26 -0700

    followed along instructor's solution to CODING CHALLENGE 3

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -533,7 +533,7 @@ console.log(isDesigner);
 
 
 /******************************
-* CODING CHALLENGE 2
+* CODING CHALLENGE 3
 */
 
 /*
@@ -553,7 +553,7 @@ In the end, John would like to have 2 arrays:
 
 GOOD LUCK :)
 */
-
+/*
 // My solution
 var billsJohn = [124, 48, 268]
 var tips = [];
@@ -577,7 +577,31 @@ tipCalculator(billsJohn[1]);
 tipCalculator(billsJohn[2]);
 
 console.log(tips, final);
+*/
 
+// Instructor's solution
+
+function tipCalculator(bill) {
+    var percentage;
+    if (bill < 50) {
+        percentage = .2;
+    } else if (bill >= 50 && bill < 200) {
+        percentage = .15;
+    } else {
+        percentage = .1;
+    }
+    return percentage * bill;
+}
+
+var bills = [124, 48, 268];
+var tips = [tipCalculator(bills[0]),
+            tipCalculator(bills[1]),
+            tipCalculator(bills[2])];
+var finalValues = [bills[0] + tips[0],
+                    bills[1] + tips[1],
+                    bills[2] + tips[2]];
+
+console.log(tips, finalValues);
 
 
 
