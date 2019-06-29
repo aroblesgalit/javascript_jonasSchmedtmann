cda597ce7c6e1e44d2fa542fcb7df191d098fecb
Alvin Galit <aroblesgalit@gmail.com>
2019-06-28 21:36:41 -0700

    learned about continue and break statements, used for loop to log properties in the john array, then did looping backwards

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -752,7 +752,7 @@ if (john.calcBMI() > mark.calcBMI()) {
 /******************************
 * Loops and iteration
 */
-
+/*
 // For loop
 for (var i = 0; i < 10; i++) {
     console.log(i);
@@ -780,10 +780,26 @@ while(i < john.length) {
     console.log(john[i]);
     i++;
 }
+*/
 
 
 // continue and break statements
+var john = ['John', 'Smith', 1990, 'designer', false];
 
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') continue;
+    console.log(john[i]);
+}
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') break;
+    console.log(john[i]);
+}
+
+// Looping backwards
+for (var i = john.length - 1; i >= 0 ; i--) {
+    console.log(john[i]);
+}
 
 
 
