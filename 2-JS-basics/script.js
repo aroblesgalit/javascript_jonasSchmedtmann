64b38939e32bd45386eeb761eea75a3f71454b92
Alvin Galit <aroblesgalit@gmail.com>
2019-06-29 18:20:23 -0700

    followed along instructor's solution to the first part of the coding challenge 5

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -846,7 +846,7 @@ GOOD LUCK :)
 */
 
 // My solution
-
+/*
 var john = {
     bill: [124, 48, 268, 180, 42],
     tips: [],
@@ -916,6 +916,40 @@ if (avgJohn > avgMark) {
 } else {
     console.log('Both family paid the same amount on average.');
 }
+*/
+
+
+// Instructor's solution
+
+var john = {
+    fullName: 'John Smith',
+    bills: [124, 48, 268, 180, 42],
+    calcTips: function() {
+        this.tips = [];
+        this.finalValues = [];
+
+        for (var i = 0; i < this.bills.length; i++) {
+            // Determine percentage based on tipping rules
+            var percentage;
+            var bill = this.bills[i];
+            
+            if (bill < 50) {
+                percentage = .2;
+            } else if (bill >= 50 && this.bills[i] < 200) {
+                percentage = .15;
+            } else {
+                percentage = .1;
+            }
+
+            // Add results to the corresponding arrays
+            this.tips[i] = bill * percentage;
+            this.finalValues[i] = bill + bill * percentage;
+        }
+    }
+}
+
+john.calcTips();
+console.log(john);
 
 
 
