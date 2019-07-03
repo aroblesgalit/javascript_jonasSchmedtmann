4e23109df131ceb0f0391a3a9fec633ed072dac1
Alvin Galit <aroblesgalit@gmail.com>
2019-07-02 20:09:21 -0700

    did some practice on the 'this' keyword

--- 3-how-JS-works/script.js
+++ 3-how-JS-works/script.js
@@ -1,6 +1,6 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
-
+/*
 // functions
 calculateAge(1965);
 
@@ -72,12 +72,51 @@ function third() {
     // console.log(c);
     console.log(a + d);
 }
-
+*/
 
 
 
 ///////////////////////////////////////
 // Lecture: The this keyword
+
+// console.log(this);
+
+/*
+calculateAge(1985);
+
+function calculateAge(year) {
+    console.log(2016 - year);
+    console.log(this);
+}
+*/
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2016 - this.yearOfBirth);
+
+        /*
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+        */
+    }
+};
+
+john.calculateAge();
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984,
+};
+
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
+
+
 
 
 
