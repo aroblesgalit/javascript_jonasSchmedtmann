75849bf9bd03e1153fa57e68b75c19864b28ad1d
Alvin Galit <aroblesgalit@gmail.com>
2019-07-24 08:53:51 -0700

    did dom manipulation by selecting the div containing the current score of player 1 and replacing it with the dice variable

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -16,10 +16,9 @@ roundScore = 0;
 activePlayer = 0;
 
 dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
 
-
-
+document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 
 
