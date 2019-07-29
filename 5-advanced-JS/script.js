03b809dd172fe6fa71fb81e33fa0c48621945b81
Alvin Galit <aroblesgalit@gmail.com>
2019-07-29 11:14:21 -0500

    created a constructor function called Person with the properties name, yearOfBirth, and job, then created instances from it, and also created method and property prototype

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -1,4 +1,5 @@
 /* 
+NOTES:
 - Every JavaScript object has a prototype property, which makes inheritance
 possible in JavaScript;
 - The prototype property of an object is where we put methods and properties
@@ -9,3 +10,84 @@ through it;
 - When a certain method (or property) is called, the search starts in the
 object itself, and if it cannot be found, the search moves on to the object's
 prototype. This continues until the method is found: prototype chain.
+*/
+
+
+// Function constructor
+
+/*
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    job: 'teacher'
+};
+*/
+
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+};
+
+Person.prototype.calculateAge = function() {
+    console.log(2019 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
+
+
+/*
+PRACTICE: Create your own constructor function and add methods and properties
+to its prototype.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
