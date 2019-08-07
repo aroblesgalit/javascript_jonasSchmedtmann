035d2afae1bc094e04fc83eb688c7d05eb2e4117
Alvin Galit <aroblesgalit@gmail.com>
2019-08-06 22:49:00 -0500

    practiced function returning functions

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -127,7 +127,7 @@ console.log(obj.city);
 */
 
 
-
+/*
 //////////////////////////////////
 // Lecture: Passing functions as arguments
 
@@ -164,6 +164,36 @@ var rates = arrayCalc(ages, maxHeartRate);
 console.log(ages);
 console.log(fullAges);
 console.log(rates);
+*/
+
+
+/////////////////////////////////
+// Lecture: Function returning functions
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('John');
+
+interviewQuestion('teacher')('Mark');
+*/
 
 
 
