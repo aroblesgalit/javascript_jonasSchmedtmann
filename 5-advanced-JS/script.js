1199ff3284f14f9bc565a4555996423c19119f34
Alvin Galit <aroblesgalit@gmail.com>
2019-08-09 00:06:26 -0500

    learned about closures

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -231,7 +231,7 @@ function game() {
 }
 game();
 */
-
+/*
 (function () {
     var score = Math.random() * 10;
     console.log(score >= 5);  
@@ -243,9 +243,25 @@ game();
     var score = Math.random() * 10;
     console.log(score >= 5 - goodLuck);  
 })(5);
+*/
 
 
 
+///////////////////////////////
+// Lecture: Closures
+
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2019 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    } 
+}
+
+var retirementUS = retirement(66);
+retirementUS(1990);
+
+retirement(66)(1990); 
 
 
 
