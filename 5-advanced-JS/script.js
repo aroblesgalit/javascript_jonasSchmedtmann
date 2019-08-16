854e1ecb5eb601493452ea12ac226314c5bebc3c
Alvin Galit <aroblesgalit@gmail.com>
2019-08-15 23:12:35 -0500

    followed along instructor's solution to part 1 of the coding challenge

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -426,7 +426,7 @@ feel more comfortable at this point).
 11. Display the score in the console. Use yet another method for this.
 
 */
-
+/*
 // My Solution
 var Question = function(question, answers, correctAnswer) {
     this.question = question;
@@ -487,7 +487,64 @@ Question.prototype.randomQuestion = function() {
 function nextQuestion() {
     question1.randomQuestion();
 }
+*/
 
+
+// Instructor's solution
+
+(function() {
+
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+    
+    Question.prototype.displayQuestion = function() {
+        console.log(this.question);
+    
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
+    
+    Question.prototype.checkAnswer = function(ans) {
+        if (ans === this.correct) {
+            console.log('Correct answer!');
+        } else {
+            console.log('Wrong answer. Try again :)');
+        }
+    }
+    
+    var q1 = new Question(
+        'Is JavaScript the coolest programming language in the world?',
+        ['Yes', 'No'],
+        0
+    );
+    
+    var q2 = new Question(
+        'What is the name of this course\'s teacher?',
+        ['John', 'Michael', 'Jonas'],
+        2
+    );
+    
+    var q3 = new Question(
+        'What does best describe coding?',
+        ['Boring', 'Hard', 'Fun', 'Tedious'],
+        2
+    );
+    
+    var questions = [q1, q2, q3];
+    
+    var n = Math.floor(Math.random() * questions.length);
+    
+    questions[n].displayQuestion();
+    
+    var answer = parseInt(prompt('Please select the correct answer.'));
+    
+    questions[n].checkAnswer(answer);
+    
+})();
 
 
 
