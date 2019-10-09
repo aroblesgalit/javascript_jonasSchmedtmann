299cdb1a15fbf0a184972ea974f866899ef476aa
Alvin Galit <aroblesgalit@gmail.com>
2019-10-08 22:25:46 -0500

    did some practice on arrow functions on ES6

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -83,9 +83,10 @@ console.log(c);
 */
 
 
+
 ///////////////////////////////////
 // Lecture: Strings
-
+/*
 let firstName = 'John';
 let lastName = 'Smith';
 const yearOfBirth = 1990;
@@ -107,6 +108,40 @@ console.log(n.endsWith('th'));
 console.log(n.includes('oh'));
 console.log(firstName.repeat(5));
 console.log(`${firstName} `.repeat(5));
+*/
+
+
+
+///////////////////////////////////
+// Lecture: Arrow functions
+
+const years = [1990, 1965, 1982, 1937];
+
+// ES5
+var ages5 = years.map(function(el) {
+    return 2019 - el;
+});
+console.log(ages5);
+
+
+// ES6
+let ages6 = years.map(el => 2019 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2019 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${age}.`
+});
+console.log(ages6);
+
+
+
+
+
 
 
 
