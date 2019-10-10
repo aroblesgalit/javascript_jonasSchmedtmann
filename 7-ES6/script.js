9e2f478d56bb77a1c3dd193d35f478d6213b3735
Alvin Galit <aroblesgalit@gmail.com>
2019-10-10 13:58:12 -0500

    practiced using the spread operator

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -264,25 +264,25 @@ console.log(retirement);
 
 ///////////////////////////////////
 // Lecture: Arrays
-
-const boxes = document.querySelectorAll('.box');
 /*
+const boxes = document.querySelectorAll('.box');
+
 // ES5 - Creating an array and changing each background color to dodgerblue
 var boxesArr5 = Array.prototype.slice.call(boxes);
 boxesArr5.forEach(function(cur) {
     cur.style.backgroundColor = 'dodgerblue';
 });
-*/
+
 
 // ES6
 const boxesArr6 = Array.from(boxes);
 boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue');
 
-/*
+
 or
 Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
-*/
-/*
+
+
 // ES5 - Creating an array and changing each text to I'm blue!
 for(var i = 0; i < boxesArr5.length; i++) {
 
@@ -293,7 +293,7 @@ for(var i = 0; i < boxesArr5.length; i++) {
     boxesArr5[i].textContent = 'I changed to blue!';
 
 }
-*/
+
 
 // ES6
 for (const cur of boxesArr6) {
@@ -320,6 +320,45 @@ console.log(ages[full.indexOf(true)]);
 // ES6
 console.log(ages.findIndex(cur => cur >= 18));
 console.log(ages.find(cur => cur >= 18));
+*/
+
+
+
+
+///////////////////////////////////
+// Lecture: Spread operator
+
+function addFourAges (a, b, c, d) {
+    return a + b + c + d;
+}
+
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1);
+
+// ES5 - if given an array
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+// ES6 - using spread operator
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+const bigFamily = [...familySmith, 'Lily', ...familyMiller]; // spreads both const
+console.log(bigFamily);
+
+
+// select html elements and change all text color to purple
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = 'purple');
+
+
 
 
 
